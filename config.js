31b02f0f5a7fcddc06a67dc2d3f2e498fcef58ab
Ariel Balter <ariel@arielbalter.com>
2018-04-21 09:37:10 -0700

    changed to github source

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var reponame = 'attoweb/';
 
 // var base_url = github_base + username + reponame + 'master/';
 var base_url = "https://raw.githubusercontent.com/Attoweb/attoweb.org/master/";
-base_url = "";
+// base_url = "";
 
 var routes =
 {
