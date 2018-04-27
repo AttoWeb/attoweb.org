188b7484817edca6eba89a6cadf5fee6d032fb8f
Ariel Balter <ariel@arielbalter.com>
2018-04-27 10:36:39 -0700

    and again because was not on master

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ var username = 'abalter/';
 var reponame = 'attoweb/';
 
 // var base_url = github_base + username + reponame + 'master/';
-var base_url = "https://raw.githubusercontent.com/Attoweb/attoweb.org/httpquery/";
+var base_url = "https://raw.githubusercontent.com/Attoweb/attoweb.org/master/";
 // base_url = "";
 
 var routes =
